b632bcdb492b5549ee409757a446e66310e3c085
SKornya <kornyakovS@gmail.com>
2022-11-30 20:54:48 +0300

    fixed

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,19 +1,14 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import { file1, file2, genDiff } from '../src/files-compare.js';
-import fs from 'fs';
-import _ from 'lodash';
-
-console.log(genDiff(file1, file2));
+import genDiff from '../src/difference-generator.js';
 
 program
-  .version("1.0.0")
+  .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'display help for command')
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
-  .action((path1, path2) => genDiff(fs.readFileSync(path1), fs.readFileSync(path2)));
+  .action((path1, path2) => console.log(genDiff(path1, path2)));
 
 program.parse();
-
---
+++ src/difference-generator.js
@@ -0,0 +1,28 @@
+import _ from 'lodash';
+import fs from 'fs';
+import yaml from 'yaml-js';
+
+const parseJSONFromPath = (path) => JSON.parse(fs.readFileSync(path));
+const parseYMLFromPath = (path) => yaml.load(fs.readFileSync(path)); // for yml files parse later
+
+export default (path1, path2) => {
+  const parsedFiles = [path1, path2]
+    .map((path) => (_.last(path.split('.')) === 'json' ? parseJSONFromPath(path) : parseYMLFromPath(path)));
+
+  const [file1, file2] = parsedFiles;
+  const keys = parsedFiles.map((file) => Object.keys(file));
+  const [keys1, keys2] = keys;
+
+  const intersectKeys = _.intersection(keys1, keys2);
+  const unionKeys = _.union(keys1, keys2).sort();
+
+  const getDiff = unionKeys.reduce((acc, key) => {
+    if (intersectKeys.includes(key)) {
+      return file1[key] === file2[key] ? `${acc}     ${key}: ${file1[key]}\n` : `${acc}   - ${key}: ${file1[key]}\n   + ${key}: ${file2[key]}\n`;
+    }
+    return keys1.includes(key) ? `${acc}   - ${key}: ${file1[key]}\n` : `${acc}   + ${key}: ${file2[key]}\n`;
+  }, '');
+
+  const diff = `{\n${getDiff}}`;
+  return diff;
+};
