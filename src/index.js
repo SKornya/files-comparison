7558870f33e68564c7b2c07f63a67685e2fb3781
SKornya <kornyakovS@gmail.com>
2022-12-10 22:20:08 +0300

    remove formatter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import _ from 'lodash';
 import getParsedFile from './parsers.js';
+import getStylishDiff from './stylish.js';
+// import getPlainDiff from './plain.js';
 
-const isObjectAndNotArray = (file, key) => typeof file[key] === 'object' && !Array.isArray(file[key]);
-
-const getDiffObj = (file1, file2) => {
+const getUnionKeys = (file1, file2) => {
   const parsedFiles = [file1, file2];
-
   const keys = parsedFiles.map((file) => Object.keys(file));
   const [keys1, keys2] = keys;
 
   const unionKeys = _.sortBy(_.union(keys1, keys2));
-  return unionKeys
+  return unionKeys;
+};
+
+const isObjectAndNotArray = (file, key) => typeof file[key] === 'object' && !Array.isArray(file[key]);
+
+const getDiffObj = (file1, file2) => {
+  const keys = getUnionKeys(file1, file2);
+  return keys
     .reduce((acc, key) => {
       if (isObjectAndNotArray(file1, key) && isObjectAndNotArray(file2, key)) {
         return { ...acc, [key]: getDiffObj(file1[key], file2[key]) };
@@ -24,11 +30,14 @@ const getDiffObj = (file1, file2) => {
     }, {});
 };
 
-export default (file1Name, file2Name) => {
+export default (file1Name, file2Name, format = 'stylish') => {
   const file1 = getParsedFile(file1Name);
   const file2 = getParsedFile(file2Name);
 
-  const diffQuoted = JSON.stringify(getDiffObj(file1, file2), null, '  ');
-  const diff = diffQuoted.replace(/"|,/g, '');
-  return diff;
+  const diffObj = getDiffObj(file1, file2);
+  if (format === 'stylish') {
+    return getStylishDiff(diffObj);
+  }
+  return '';
+//     : getPlainDiff(file1, file2, unionKeys);
 };
