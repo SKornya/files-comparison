80e35594374052906a623784dcc8afbf8fa6d28b
SKornya <kornyakovS@gmail.com>
2022-12-11 21:06:50 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -1,43 +1,10 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import getParsedFile from './parsers.js';
-import getStylishDiff from './formatters/stylish.js';
-// import getPlainDiff from './plain.js';
+import getFormatter from './formatters/index.js';
 
-const getUnionKeys = (file1, file2) => {
-  const parsedFiles = [file1, file2];
-  const keys = parsedFiles.map((file) => Object.keys(file));
-  const [keys1, keys2] = keys;
+export default (file1Path, file2Path, format = 'stylish') => {
+  const file1 = getParsedFile(file1Path);
+  const file2 = getParsedFile(file2Path);
 
-  const unionKeys = _.sortBy(_.union(keys1, keys2));
-  return unionKeys;
-};
-
-const isObjectAndNotArray = (file, key) => typeof file[key] === 'object' && !Array.isArray(file[key]);
-
-const getDiffObj = (file1, file2) => {
-  const keys = getUnionKeys(file1, file2);
-  return keys
-    .reduce((acc, key) => {
-      if (isObjectAndNotArray(file1, key) && isObjectAndNotArray(file2, key)) {
-        return { ...acc, [key]: getDiffObj(file1[key], file2[key]) };
-      }
-      if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
-        return file1[key] === file2[key]
-          ? { ...acc, [`  ${key}`]: file1[key] } : { ...acc, [`- ${key}`]: file1[key], [`+ ${key}`]: file2[key] };
-      }
-      return Object.hasOwn(file1, key)
-        ? { ...acc, [`- ${key}`]: file1[key] } : { ...acc, [`+ ${key}`]: file2[key] };
-    }, {});
-};
-
-export default (file1Name, file2Name, format = 'stylish') => {
-  const file1 = getParsedFile(file1Name);
-  const file2 = getParsedFile(file2Name);
-
-  const diffObj = getDiffObj(file1, file2);
-  if (format === 'stylish') {
-    return getStylishDiff(diffObj);
-  }
-  return '';
-//     : getPlainDiff(file1, file2, unionKeys);
+  return getFormatter(file1, file2, format);
 };
