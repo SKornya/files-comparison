54252efdc1ab0cb66e63bb2ce97b4b2e2cebf4f2
SKornya <kornyakovS@gmail.com>
2022-12-11 22:26:51 +0300

    add choice to json formater

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,5 +1,6 @@
 import getStylishDiff from './stylish.js';
 import getPlainDiff from './plain.js';
+import getJSONDiff from './json-formater.js';
 
 export default (file1, file2, format) => {
   switch (format) {
@@ -7,6 +8,8 @@ export default (file1, file2, format) => {
       return getStylishDiff(file1, file2);
     case 'plain':
       return getPlainDiff(file1, file2);
+    case 'json':
+      return getJSONDiff(file1, file2);
     default:
       throw new Error('Unknow format');
   }
