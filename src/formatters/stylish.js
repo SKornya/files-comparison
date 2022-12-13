c67cb9b41f9480935e5d2e8a70406be529eafa5c
SKornya <kornyakovS@gmail.com>
2022-12-13 20:10:13 +0300

    remove line in stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -18,21 +18,20 @@ const blankMark = '    ';
 
 const stylish = (diff) => {
   const formater = (data, depth = 0) => {
-    const formatted = data
-      .map((key) => {
-        switch (key.type) {
-          case 'PARENT':
-            return `${indent(depth)}${blankMark}${key.name}: ${formater(key.children, depth + 1)}`;
-          case 'ADDED':
-            return `${indent(depth)}${plusMark}${key.name}: ${getValue(key.value, depth)}`;
-          case 'DELETED':
-            return `${indent(depth)}${minusMark}${key.name}: ${getValue(key.value, depth)}`;
-          case 'UNCHANGED':
-            return `${indent(depth)}${blankMark}${key.name}: ${getValue(key.value, depth)}`;
-          default:
-            return `${indent(depth)}${minusMark}${key.name}: ${getValue(key.value[0], depth)}\n${indent(depth)}${plusMark}${key.name}: ${getValue(key.value[1], depth)}`;
-        }
-      });
+    const formatted = data.map((key) => {
+      switch (key.type) {
+        case 'PARENT':
+          return `${indent(depth)}${blankMark}${key.name}: ${formater(key.children, depth + 1)}`;
+        case 'ADDED':
+          return `${indent(depth)}${plusMark}${key.name}: ${getValue(key.value, depth)}`;
+        case 'DELETED':
+          return `${indent(depth)}${minusMark}${key.name}: ${getValue(key.value, depth)}`;
+        case 'UNCHANGED':
+          return `${indent(depth)}${blankMark}${key.name}: ${getValue(key.value, depth)}`;
+        default:
+          return `${indent(depth)}${minusMark}${key.name}: ${getValue(key.value[0], depth)}\n${indent(depth)}${plusMark}${key.name}: ${getValue(key.value[1], depth)}`;
+      }
+    });
 
     return `{\n${formatted.join('\n')}\n${indent(depth)}}`;
   };
