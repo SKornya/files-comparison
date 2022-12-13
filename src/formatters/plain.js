c4a590e0f94eb63d40dbe28d810b652cee8e0bbd
SKornya <kornyakovS@gmail.com>
2022-12-13 19:39:45 +0300

    fixed plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
 const getValue = (value) => {
-  if (typeof value === 'object') {
+  if (_.isObject(value)) {
     return '[complex value]';
   }
   return typeof value === 'string' ? `'${value}'` : value;
@@ -16,7 +16,7 @@ const plainFormater = (diff, path = '') => diff
       return `Property '${path}${key.name}' was added with value: ${getValue(key.value)}`;
     }
     if (key.type === 'DELETED') {
-      return `Property '${path}${key.name}' was deleted`;
+      return `Property '${path}${key.name}' was removed`;
     }
     if (key.type === 'CHANGED') {
       return `Property '${path}${key.name}' was updated. From ${getValue(key.value[0])} to ${getValue(key.value[1])}`;
