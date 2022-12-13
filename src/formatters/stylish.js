7426c161c2c65e043f6d06066ee410bbadb9db63
SKornya <kornyakovS@gmail.com>
2022-12-13 14:24:20 +0300

    fixed fixture

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,23 +1,23 @@
 const stylishFormater = (diff) => diff
   .reduce((acc, key) => {
     if (key.type === 'PARENT') {
-      return { ...acc, [key.name]: stylishFormater(key.children) };
+      return { ...acc, [`  ${key.name}`]: stylishFormater(key.children) };
     }
     if (key.type === 'ADDED') {
-      return { ...acc, [`+ ${key.name}`]: key.value };
+      return { ...acc, [`  + ${key.name}`]: key.value };
     }
     if (key.type === 'DELETED') {
-      return { ...acc, [`- ${key.name}`]: key.value };
+      return { ...acc, [`  - ${key.name}`]: key.value };
     }
     if (key.type === 'UNCHANGED') {
-      return { ...acc, [`  ${key.name}`]: key.value };
+      return { ...acc, [`    ${key.name}`]: key.value };
     }
-    return { ...acc, [`- ${key.name}`]: key.value[0], [`+ ${key.name}`]: key.value[1] };
+    return { ...acc, [`  - ${key.name}`]: key.value[0], [`  + ${key.name}`]: key.value[1] };
   }, {});
 
 export default (diff) => {
   const diffObj = stylishFormater(diff);
-  const diffQuoted = JSON.stringify(diffObj, null, '    ');
+  const diffQuoted = JSON.stringify(diffObj, null, '  ');
 
   return diffQuoted.replace(/"|,/g, '');
 };
