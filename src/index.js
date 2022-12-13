b9fe21c80b7e971cd94776a220c4148c66be6f0b
SKornya <kornyakovS@gmail.com>
2022-12-13 10:02:36 +0300

    clean

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import getDiff from './parsers1.js';
+import getDiff from './parsers.js';
 import format from './formatters/index.js';
-// import _ from 'lodash';
-// import getParsedFile from './parsers.js';
-// import getFormatter from './formatters/index.js';
 
 export default (file1Path, file2Path, formater = 'stylish') => {
   const diff = getDiff(file1Path, file2Path);
+
   return format(formater, diff);
-  // return getFormatter(file1, file2, format);
 };
