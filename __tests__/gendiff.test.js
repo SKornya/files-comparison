2f970937ded76053be0ecebc722a46f8bbeb8bfe
SKornya <kornyakovS@gmail.com>
2022-12-15 11:01:25 +0300

    add test for default format

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,40 @@
+import { test, expect } from '@jest/globals';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import genDiff from '../src/index.js';
+
+const filePath = fileURLToPath(import.meta.url);
+const dirPath = dirname(filePath);
+
+const getFixturePath = (fileName) => path.join(dirPath, '..', '__fixtures__', fileName);
+const fileRead = (file) => fs.readFileSync(getFixturePath(file), 'utf-8');
+
+const expectedStylish = fileRead('expectedStylish.txt');
+const expectedPlain = fileRead('expectedPlain.txt');
+const expectedJSON = fileRead('expectedJSON.txt');
+
+test('default format value', () => {
+  const file1Path = '__fixtures__/file1.json';
+  const file2Path = '__fixtures__/file2.json';
+
+  expect(genDiff(file1Path, file2Path)).toEqual(expectedStylish);
+});
+
+test.each([
+  ['__fixtures__/file1.json', '__fixtures__/file2.json', 'stylish', expectedStylish],
+  ['__fixtures__/file1.json', '__fixtures__/file2.json', 'plain', expectedPlain],
+  ['__fixtures__/file1.json', '__fixtures__/file2.json', 'json', expectedJSON],
+
+  ['__fixtures__/file1.yml', '__fixtures__/file2.yaml', 'stylish', expectedStylish],
+  ['__fixtures__/file1.yml', '__fixtures__/file2.yaml', 'plain', expectedPlain],
+  ['__fixtures__/file1.yml', '__fixtures__/file2.yaml', 'json', expectedJSON],
+
+  ['__fixtures__/file1.json', '__fixtures__/file2.yaml', 'stylish', expectedStylish],
+  ['__fixtures__/file1.json', '__fixtures__/file2.yaml', 'plain', expectedPlain],
+  ['__fixtures__/file1.json', '__fixtures__/file2.yaml', 'json', expectedJSON],
+])('test\n%s\n%s\n%s', (file1Path, file2Path, format, expFile) => {
+  const input = genDiff(file1Path, file2Path, format);
+
+  expect(input).toEqual(expFile);
+});
