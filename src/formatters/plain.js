8ee3c19e23ca3525f04b4c08a9379d0794bc8bfe
SKornya <kornyakovS@gmail.com>
2022-12-16 22:50:10 +0300

    transfer 'switch' from getFormatted to own func

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -7,23 +7,28 @@ const getValue = (value) => {
   return typeof value === 'string' ? `'${value}'` : value;
 };
 
+const getPropertyDifference = (key, path) => {
+  switch (key.type) {
+    case 'ADDED':
+      return `Property '${path}' was added with value: ${getValue(key.value)}`;
+    case 'DELETED':
+      return `Property '${path}' was removed`;
+    case 'CHANGED':
+      return `Property '${path}' was updated. From ${getValue(key.value[0])} to ${getValue(key.value[1])}`;
+    case 'UNCHANGED':
+      return '';
+    default:
+      throw new Error('unknown key type');
+  }
+};
+
 const getFormatted = (diff, path = '') => diff
   .flatMap((key) => {
     const currentPath = `${path}${key.name}`;
-    switch (key.type) {
-      case 'PARENT':
-        return getFormatted(key.children, `${currentPath}.`);
-      case 'ADDED':
-        return `Property '${currentPath}' was added with value: ${getValue(key.value)}`;
-      case 'DELETED':
-        return `Property '${currentPath}' was removed`;
-      case 'CHANGED':
-        return `Property '${currentPath}' was updated. From ${getValue(key.value[0])} to ${getValue(key.value[1])}`;
-      case 'UNCHANGED':
-        return '';
-      default:
-        throw new Error('unknown key type');
+    if (key.type === 'PARENT') {
+      return getFormatted(key.children, `${currentPath}.`);
     }
+    return getPropertyDifference(key, currentPath);
   });
 
 export default (diff) => {
